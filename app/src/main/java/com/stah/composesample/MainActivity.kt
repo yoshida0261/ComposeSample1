7d9b794837632944aa2100e36076b73759495ed7
yoshida0261 <yoshida261@gmail.com>
2021-09-20 00:29:55 +0900

    不要なコード削除

--- app/src/main/java/com/stah/composesample/MainActivity.kt
+++ app/src/main/java/com/stah/composesample/MainActivity.kt
@@ -3,36 +3,13 @@ package com.stah.composesample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.stah.composesample.ui.theme.ComposeSampleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeSampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            Text(text = "Hello world2")
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeSampleTheme {
-        Greeting("Android")
     }
 }
