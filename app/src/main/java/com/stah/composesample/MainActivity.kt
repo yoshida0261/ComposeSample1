8d34daeb142de2648ade8394d2df88b6645e7a1e
yoshida0261 <yoshida261@gmail.com>
2021-09-20 01:39:43 +0900

    count up

--- app/src/main/java/com/stah/composesample/MainActivity.kt
+++ app/src/main/java/com/stah/composesample/MainActivity.kt
@@ -3,13 +3,40 @@ package com.stah.composesample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Hello world2")
+            CountUp()
+        }
+    }
+
+    @Composable
+    fun HelloWorld() {
+        Text(text = "Hello world2")
+    }
+
+    @Composable
+    fun CountUp() {
+        var count: Int by remember {
+            mutableStateOf(0)
+        }
+
+        Column {
+            Text("$count")
+            Button(onClick = { count += 1 }) {
+                Text(text = "Count up")
+            }
         }
     }
 }
+
